2d14559f92c30e83acbd226a8a90811b861ea4c1
vincentRouget <108070360+vincentRouget@users.noreply.github.com>
2023-12-15 00:42:02 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular1';
+  title = 'Bienvenue sur le site de Vincent Rouget !';
 }
